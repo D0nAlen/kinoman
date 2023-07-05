92db32ad8ea3d366892adec67f08738405dca0e2
Alena <vasilevnalena.2012@gmail.com>
2023-07-05 08:26:01 +0400

    Update main.js

---
+++ project/src/main.js
@@ -0,0 +1,126 @@
+"use strict";
+
+const createHeaderProfileTemplate = () => {
+  return `
+    <section class="header__profile profile">
+    <p class="profile__rating">Movie Buff</p>
+    <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+  </section>
+    `;
+};
+
+const createMenuTemplate = () => {
+  return `
+  <nav class="main-navigation">
+  <div class="main-navigation__items">
+    <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
+    <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
+    <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
+    <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
+  </div>
+  <a href="#stats" class="main-navigation__additional">Stats</a>
+</nav>
+    `;
+};
+
+const createFilterTemplate = () => {
+  return `
+  <ul class="sort">
+  <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
+  <li><a href="#" class="sort__button">Sort by date</a></li>
+  <li><a href="#" class="sort__button">Sort by rating</a></li>
+</ul>
+      `;
+};
+
+const createStatisticTemplate = () => {
+  return `
+  <section class="statistic">
+  <p class="statistic__rank">
+    Your rank
+    <img class="statistic__img" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+    <span class="statistic__rank-label">Sci-Fighter</span>
+  </p>
+
+  <form action="https://echo.htmlacademy.ru/" method="get" class="statistic__filters">
+    <p class="statistic__filters-description">Show stats:</p>
+
+    <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-all-time" value="all-time" checked>
+    <label for="statistic-all-time" class="statistic__filters-label">All time</label>
+
+    <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-today" value="today">
+    <label for="statistic-today" class="statistic__filters-label">Today</label>
+
+    <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-week" value="week">
+    <label for="statistic-week" class="statistic__filters-label">Week</label>
+
+    <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-month" value="month">
+    <label for="statistic-month" class="statistic__filters-label">Month</label>
+
+    <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-year" value="year">
+    <label for="statistic-year" class="statistic__filters-label">Year</label>
+  </form>
+
+  <ul class="statistic__text-list">
+    <li class="statistic__text-item">
+      <h4 class="statistic__item-title">You watched</h4>
+      <p class="statistic__item-text">22 <span class="statistic__item-description">movies</span></p>
+    </li>
+    <li class="statistic__text-item">
+      <h4 class="statistic__item-title">Total duration</h4>
+      <p class="statistic__item-text">130 <span class="statistic__item-description">h</span> 22 <span class="statistic__item-description">m</span></p>
+    </li>
+    <li class="statistic__text-item">
+      <h4 class="statistic__item-title">Top genre</h4>
+      <p class="statistic__item-text">Sci-Fi</p>
+    </li>
+  </ul>
+
+  <div class="statistic__chart-wrap">
+    <canvas class="statistic__chart" width="1000"></canvas>
+  </div>
+
+</section>
+      `;
+};
+
+const render = (container, template, place = "beforeend") => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const siteMainElement = document.querySelector(".main");
+const siteHeaderElement = siteMainElement.querySelector(".header__profile profile");
+
+render(siteHeaderElement, createHeaderProfileTemplate());
+// render(siteMainElement, createFilterTemplate());
+
+const createCardFilmTemplate = () => {
+  return `
+    
+        `;
+};
+
+const createShowMoreButtonTemplate = () => {
+  return `
+    
+        `;
+};
+
+const createCardTopRatedTemplate = () => {
+  return `
+    
+        `;
+};
+
+const createCardMostCommentedTemplate = () => {
+  return `
+    
+        `;
+};
+
+//   popup
+const createDetailedInformationFilmTemplate = () => {
+  return `
+    
+        `;
+};
