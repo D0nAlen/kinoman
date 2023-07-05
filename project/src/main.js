c43e81dc194a97b0679175c0b67481ce39352171
vasilevnalena <vasilevnalena.2012@gmail.com>
2023-07-05 10:20:53 +0400

    Added statistic page, and cards films on main page.

--- project/src/main.js
+++ project/src/main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const CARD_COUNT = 5;
+
 const createHeaderProfileTemplate = () => {
   return `
     <section class="header__profile profile">
@@ -33,55 +35,39 @@ const createFilterTemplate = () => {
       `;
 };
 
-const createStatisticTemplate = () => {
+const filmsContainerTemplate = () => {
   return `
-  <section class="statistic">
-  <p class="statistic__rank">
-    Your rank
-    <img class="statistic__img" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
-    <span class="statistic__rank-label">Sci-Fighter</span>
+  <section class="films">
+  <section class="films-list">
+    <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+  
+    <div class="films-list__container">
+  `;
+};
+const cardFilmTemplate = () => {
+  return `
+  <article class="film-card">
+  <h3 class="film-card__title">Sagebrush Trail</h3>
+  <p class="film-card__rating">3.2</p>
+  <p class="film-card__info">
+    <span class="film-card__year">1933</span>
+    <span class="film-card__duration">54m</span>
+    <span class="film-card__genre">Western</span>
   </p>
-
-  <form action="https://echo.htmlacademy.ru/" method="get" class="statistic__filters">
-    <p class="statistic__filters-description">Show stats:</p>
-
-    <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-all-time" value="all-time" checked>
-    <label for="statistic-all-time" class="statistic__filters-label">All time</label>
-
-    <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-today" value="today">
-    <label for="statistic-today" class="statistic__filters-label">Today</label>
-
-    <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-week" value="week">
-    <label for="statistic-week" class="statistic__filters-label">Week</label>
-
-    <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-month" value="month">
-    <label for="statistic-month" class="statistic__filters-label">Month</label>
-
-    <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-year" value="year">
-    <label for="statistic-year" class="statistic__filters-label">Year</label>
+  <img src="./images/posters/sagebrush-trail.jpg" alt="" class="film-card__poster">
+  <p class="film-card__description">Sentenced for a murder he did not commit, John Brant escapes from prison determined to find the real killer. By chance Brant's narrow escap…</p>
+  <a class="film-card__comments">89 comments</a>
+  <form class="film-card__controls">
+    <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist film-card__controls-item--active">Add to watchlist</button>
+    <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
+    <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
   </form>
+</article>
+          `;
+};
 
-  <ul class="statistic__text-list">
-    <li class="statistic__text-item">
-      <h4 class="statistic__item-title">You watched</h4>
-      <p class="statistic__item-text">22 <span class="statistic__item-description">movies</span></p>
-    </li>
-    <li class="statistic__text-item">
-      <h4 class="statistic__item-title">Total duration</h4>
-      <p class="statistic__item-text">130 <span class="statistic__item-description">h</span> 22 <span class="statistic__item-description">m</span></p>
-    </li>
-    <li class="statistic__text-item">
-      <h4 class="statistic__item-title">Top genre</h4>
-      <p class="statistic__item-text">Sci-Fi</p>
-    </li>
-  </ul>
-
-  <div class="statistic__chart-wrap">
-    <canvas class="statistic__chart" width="1000"></canvas>
-  </div>
-
-</section>
-      `;
+const createShowMoreButtonTemplate = () => {
+  return `<button class="films-list__show-more">Show more</button>`;
 };
 
 const render = (container, template, place = "beforeend") => {
@@ -96,37 +82,31 @@ render(siteMainElement, createMenuTemplate());
 const siteFilterElement = siteMainElement.querySelector(".main-navigation");
 render(siteMainElement, createFilterTemplate());
 
-// const siteStatisticElement = siteMainElement.querySelector(".main-navigation");
-// render(siteMainElement, createStatisticTemplate());
+render(siteMainElement, filmsContainerTemplate());
+const cardFilmElement = siteMainElement.querySelector(".films-list__container");
+for (let i = 0; i < CARD_COUNT; i++) {
+  render(cardFilmElement, cardFilmTemplate());
+}
+
+render(siteMainElement, createShowMoreButtonTemplate());
 
 
-// const createCardFilmTemplate = () => {
-//   return `
-    
-//         `;
-// };
-
-// const createShowMoreButtonTemplate = () => {
-//   return `
-    
-//         `;
-// };
 
 // const createCardTopRatedTemplate = () => {
 //   return `
-    
+
 //         `;
 // };
 
 // const createCardMostCommentedTemplate = () => {
 //   return `
-    
+
 //         `;
 // };
 
 //   popup
 // const createDetailedInformationFilmTemplate = () => {
 //   return `
-    
+
 //         `;
 // };
