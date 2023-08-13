a747d161e169596a79dfec07d62bbd5508c06831
Alena <vasilevnalena.2012@gmail.com>
2023-08-13 10:52:24 +0400

    Updated button All

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ import { controlsTemplate } from "./components/controls.js";
 import { generateTopRatedFilms } from "./mock/cardTopRated.js";
 import { generateMostCommentedFilms } from "./mock/cardMostCommented.js";
 import { render } from "./mock/render.js";
+import { defaultCardOutput } from "./mock/allButton.js";
 import { allButtonElement } from "./mock/allButton.js";
 
 const CARD_FILMS_COUNT = 20;
@@ -43,77 +44,14 @@ render(siteMainElement, filmsContainerTemplate());
 
 const filmsContainer = siteMainElement.querySelector(".films");
 
+defaultCardOutput(siteMainElement);
+
 allButtonElement(siteMainElement);
-// const filmsContainer = siteMainElement.querySelector(".films");
-// const filmsListContainer = filmsContainer.querySelector(
-//   ".films-list__container"
-// );
 
-// // Default cards rendering
-// for (let i = 0; i < SHOWING_FILMS_COUNT_ON_START; i++) {
-//   render(filmsListContainer, cardFilmTemplate(films[i]));
-// }
-
-// // button "Show more"
-// const filmsList = filmsContainer.querySelector(".films-list");
-// render(filmsList, showMoreButtonTemplate());
-// let showMoreButton = filmsList.querySelector(`.films-list__show-more`);
-
-// // let showingFilmsCount = SHOWING_FILMS_COUNT_ON_START;
-// // let cardFilmsCount = CARD_FILMS_COUNT;
-// // let showingFilmsCountByButton = SHOWING_FILMS_COUNT_BY_BUTTON;
-
-// const showMoreButtonElement = () => {
-//   let showingFilmsCount = SHOWING_FILMS_COUNT_ON_START;
-//   let cardFilmsCount = CARD_FILMS_COUNT;
-//   let showingFilmsCountByButton = SHOWING_FILMS_COUNT_BY_BUTTON;
-
-//   showMoreButton.addEventListener(`click`, () => {
-//     const prevFilmsCount = showingFilmsCount;
-//     let remainingCards = cardFilmsCount - showingFilmsCount;
-//     if (remainingCards < showingFilmsCountByButton)
-//       showingFilmsCountByButton = remainingCards;
-//     showingFilmsCount = showingFilmsCount + showingFilmsCountByButton;
-//     films
-//       .slice(prevFilmsCount, showingFilmsCount)
-//       .forEach((film) =>
-//         render(filmsListContainer, cardFilmTemplate(film), `beforeend`)
-//       );
-
-//     if (showingFilmsCount >= cardFilmsCount) {
-//       showMoreButton.remove();
-//     }
-//   });
-// };
-
-// showMoreButtonElement();
-
-// #All
-// const allButton = document.getElementById("all");
-// allButton.addEventListener(`click`, () => {
-//   let cardFilmsCount = CARD_FILMS_COUNT;
-//   let showingFilmsCount = SHOWING_FILMS_COUNT_ON_START;
-//   let showingFilmsCountByButton = SHOWING_FILMS_COUNT_BY_BUTTON;
-//   if (cardFilmsCount <= showingFilmsCount) {
-//     showingFilmsCount = cardFilmsCount;
-//     showMoreButton.remove();
-//   }
-
-//   filmsListContainer.innerHTML = "";
-
-//   for (let i = 0; i < showingFilmsCount; i++) {
-//     render(filmsListContainer, cardFilmTemplate(films[i]));
-//   }
-//   const controlsCardFilm = filmsListContainer.querySelectorAll(".film-card");
-//   controlsCardFilm.forEach((film) => render(film, controlsTemplate()));
-//   // showMoreButtonRendering();
-// });
 
 
 
 // !!! У каждого раздела своя кнопка Show more, со своей логикой и списком фильмов.
-// Иначе кнопка Show more(по умолч.) с Main.js перебивает корректный вывод вывод карточек при нажатии кнопки меню.
-// Добавить раздел default(~#All),для вывода карточек и show more по умолчанию.
 
 // #Watchlist
 // const watchlistButton = document.getElementById("Watchlist");
--- src/mock/allButton.js
+++ src/mock/allButton.js
@@ -3,6 +3,7 @@ import { render } from "./render";
 import { cardFilmTemplate } from "../components/cardFilm";
 import { showMoreButtonTemplate } from "../components/showMoreButton";
 import { generateFilms } from "./cardFilm";
+import { controlsTemplate } from "../components/controls";
 
 const CARD_FILMS_COUNT = 20;
 const SHOWING_FILMS_COUNT_ON_START = 5;
@@ -10,11 +11,13 @@ const SHOWING_FILMS_COUNT_BY_BUTTON = 5;
 
 const films = generateFilms(CARD_FILMS_COUNT);
 
-export const allButtonElement = (siteMainElement) => {
+// default card output
+export const defaultCardOutput = (siteMainElement) => {
   const filmsContainer = siteMainElement.querySelector(".films");
-  const filmsListContainer = filmsContainer.querySelector(".films-list__container");
+  const filmsListContainer = filmsContainer.querySelector(
+    ".films-list__container"
+  );
 
-  // Default cards rendering
   for (let i = 0; i < SHOWING_FILMS_COUNT_ON_START; i++) {
     render(filmsListContainer, cardFilmTemplate(films[i]));
   }
@@ -25,6 +28,37 @@ export const allButtonElement = (siteMainElement) => {
   let showMoreButton = filmsList.querySelector(`.films-list__show-more`);
 
   showMoreButtonElement(showMoreButton, filmsListContainer);
+};
+
+// button #All
+export const allButtonElement = (siteMainElement) => {
+  const allButton = document.getElementById("all");
+  allButton.addEventListener(`click`, () => {
+    const filmsContainer = siteMainElement.querySelector(".films");
+    const filmsListContainer = filmsContainer.querySelector(
+      ".films-list__container"
+    );
+
+    let cardFilmsCount = CARD_FILMS_COUNT;
+    let showingFilmsCount = SHOWING_FILMS_COUNT_ON_START;
+    if (cardFilmsCount <= showingFilmsCount) {
+      showingFilmsCount = cardFilmsCount;
+      showMoreButton.remove();
+    }
+
+    filmsListContainer.innerHTML = "";
+
+    for (let i = 0; i < showingFilmsCount; i++) {
+      render(filmsListContainer, cardFilmTemplate(films[i]));
+    }
+    const controlsCardFilm = filmsListContainer.querySelectorAll(".film-card");
+    controlsCardFilm.forEach((film) => render(film, controlsTemplate()));
+
+    // // button "Show more"
+    // const filmsList = filmsContainer.querySelector(".films-list");
+    // render(filmsList, showMoreButtonTemplate());
+    // let showMoreButton = filmsList.querySelector(`.films-list__show-more`);
+  });
 };
 
 const showMoreButtonElement = (showMoreButton, filmsListContainer) => {
