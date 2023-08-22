f92aaa6e66be450158337e127776805d53b90fa9
Alena <vasilevnalena.2012@gmail.com>
2023-08-22 10:14:02 +0400

    Fixed button #allButton (full rendering of changing elements)

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import { headerProfileTemplate } from "./components/headerProfile.js";
 import { menuTemplate } from "./components/menu.js";
 import { filterTemplate } from "./components/filter.js";
 import { filmsContainerTemplate } from "./components/filmsList.js";
-import { showMoreButtonTemplate } from "./components/showMoreButton.js";
 import { topRatedContainerTemplate } from "./components/topRatedContainer.js";
 import { cardTopRatedTemplate } from "./components/cardTopRated.js";
 import { mostCommentedContainerTemplate } from "./components/mostCommentedContainer.js";
@@ -14,15 +13,13 @@ import { generateMostCommentedFilms } from "./mock/cardMostCommented.js";
 import { render } from "./mock/render.js";
 import { defaultCardOutput } from "./mock/allButton.js";
 import { allButtonElement } from "./mock/allButton.js";
-import { generateFilms } from "./mock/cardFilm.js";
-import { FILMS_CARDS } from "./const.js";
+import { watchlistButtonElement } from "./mock/watchlistButton.js";
 
 // const CARD_FILMS_COUNT = 15;
 const SHOWING_FILMS_COUNT_ON_START = 5;
 const SHOWING_FILMS_COUNT_BY_BUTTON = 5;
 const CARD__TOP_RATED_COUNT = 2;
 const CARD__MOST_COMMENTED_COUNT = 2;
-// let isShowMoreButtonExist = true;
 const siteHeaderElement = document.querySelector(".header");
 const siteMainElement = document.querySelector(".main");
 
@@ -51,7 +48,7 @@ defaultCardOutput(siteMainElement);
 
 allButtonElement(siteMainElement);
 
-// watchlistButtonElement(siteMainElement, isShowMoreButtonExist, showMoreButton);
+watchlistButtonElement(siteMainElement);
 
 // !!! У каждого раздела своя кнопка Show more, со своей логикой и списком фильмов.
 
--- src/mock/watchlistButton.js
+++ src/mock/watchlistButton.js
@@ -4,65 +4,49 @@ import { showMoreButtonTemplate } from "../components/showMoreButton.js";
 import { generateFilms } from "./cardFilm.js";
 import { controlsTemplate } from "../components/controls.js";
 import { WATCHLIST_CARDS } from "../const.js";
+import { filmsContainerTemplate } from "../components/filmsList.js";
 
-const WATCHLIST_FILMS_COUNT = 10;
+const WATCHLIST_FILMS_COUNT = 7;
 const SHOWING_FILMS_COUNT_ON_START = 5;
 const SHOWING_FILMS_COUNT_BY_BUTTON = 5;
 
 const watchlistFilms = generateFilms(WATCHLIST_CARDS, WATCHLIST_FILMS_COUNT);
 
 // #Watchlist
-export const watchlistButtonElement = (siteMainElement, isShowMoreButtonExist) => {
-  let cardFilmsCount = WATCHLIST_FILMS_COUNT; //10
-  let showingFilmsCount = SHOWING_FILMS_COUNT_ON_START; //5
-  let showingFilmsCountByButton = SHOWING_FILMS_COUNT_BY_BUTTON; //5
+export const watchlistButtonElement = (siteMainElement) => {
   const watchlistButton = document.getElementById("Watchlist");
-  const filmsContainer = siteMainElement.querySelector(".films");
-  const filmsListContainer = filmsContainer.querySelector(
-    ".films-list__container"
-  );
 
   watchlistButton.addEventListener(`click`, () => {
-    // if (cardFilmsCount <= showingFilmsCount) {
-    //   showingFilmsCount = cardFilmsCount;
-    //   showMoreButton.remove();
-    //   isShowMoreButtonExist = false;
-    // }
+    let filmsContainer = siteMainElement.querySelector(".films");
+    filmsContainer.remove();
+    render(siteMainElement, filmsContainerTemplate());
+    filmsContainer = siteMainElement.querySelector(".films");
 
+    const filmsListContainer = filmsContainer.querySelector(
+      ".films-list__container"
+    );
+    const filmsList = filmsContainer.querySelector(".films-list");
+    for (let i = 0; i < SHOWING_FILMS_COUNT_ON_START; i++) {
+      render(filmsListContainer, cardFilmTemplate(watchlistFilms[i]));
+    }
 
-    let remainingCards = cardFilmsCount - showingFilmsCount;
-    if (remainingCards < showingFilmsCountByButton)
-      showingFilmsCountByButton = remainingCards;
-    showingFilmsCount = showingFilmsCount + showingFilmsCountByButton;
-    filmsListContainer.innerHTML = "";
-
-    // for (let i = 0; i < showingFilmsCount; i++) {
-    //   render(filmsListContainer, cardFilmTemplate(watchlistFilms[i]));
-    // }
-    // const controlsCardFilm = filmsListContainer.querySelectorAll(".film-card");
-    // controlsCardFilm.forEach((film) => render(film, controlsTemplate()));
-
-    // // button "Show more"
-    // const filmsList = filmsContainer.querySelector(".films-list");
-    // let showMoreButton = filmsList.querySelector(`.films-list__show-more`);
-    // if (!isShowMoreButtonExist) {
-    //   render(filmsList, showMoreButtonTemplate());
-    //   showMoreButtonElement(showMoreButton, filmsListContainer, isShowMoreButtonExist);
-    // }
+    // button "Show more"
+    render(filmsList, showMoreButtonTemplate());
+    showMoreButtonElement(filmsListContainer, filmsList);
   });
 };
 
-const showMoreButtonElement = (showMoreButton, filmsListContainer,isShowMoreButtonExist) => {
-  let showingFilmsCount = SHOWING_FILMS_COUNT_ON_START; //5
-  let cardFilmsCount = WATCHLIST_FILMS_COUNT; //10
-  let showingFilmsCountByButton = SHOWING_FILMS_COUNT_BY_BUTTON; //5
+const showMoreButtonElement = (filmsListContainer, filmsList) => {
+  let showingFilmsCount = SHOWING_FILMS_COUNT_ON_START;
+  let cardFilmsCount = WATCHLIST_FILMS_COUNT;
+  let showingFilmsCountByButton = SHOWING_FILMS_COUNT_BY_BUTTON;
+  let showMoreButton = filmsList.querySelector(`.films-list__show-more`);
 
-  // !!! Неправильно считаются карточки при выводе
   showMoreButton.addEventListener(`click`, () => {
     const prevFilmsCount = showingFilmsCount;
-    let remainingCards = cardFilmsCount - showingFilmsCount;
-    if (remainingCards < showingFilmsCountByButton)
-      showingFilmsCountByButton = remainingCards;
+    // let remainingCards = cardFilmsCount - showingFilmsCount;
+    // if (remainingCards < showingFilmsCountByButton)
+    //   showingFilmsCountByButton = remainingCards;
     showingFilmsCount = showingFilmsCount + showingFilmsCountByButton;
     watchlistFilms
       .slice(prevFilmsCount, showingFilmsCount)
@@ -70,9 +54,75 @@ const showMoreButtonElement = (showMoreButton, filmsListContainer,isShowMoreButt
         render(filmsListContainer, cardFilmTemplate(film), `beforeend`)
       );
 
+    const controlsCardFilm = filmsListContainer.querySelectorAll(".film-card");
+    controlsCardFilm.forEach((film) => render(film, controlsTemplate()));
+
     if (showingFilmsCount >= cardFilmsCount) {
       showMoreButton.remove();
-      isShowMoreButtonExist = false;
     }
   });
 };
+
+// export const watchlistButtonElement = (siteMainElement, isShowMoreButtonExist) => {
+//   let cardFilmsCount = WATCHLIST_FILMS_COUNT; //10
+//   let showingFilmsCount = SHOWING_FILMS_COUNT_ON_START; //5
+//   let showingFilmsCountByButton = SHOWING_FILMS_COUNT_BY_BUTTON; //5
+//   const watchlistButton = document.getElementById("Watchlist");
+//   const filmsContainer = siteMainElement.querySelector(".films");
+//   const filmsListContainer = filmsContainer.querySelector(
+//     ".films-list__container"
+//   );
+
+//   watchlistButton.addEventListener(`click`, () => {
+//     // if (cardFilmsCount <= showingFilmsCount) {
+//     //   showingFilmsCount = cardFilmsCount;
+//     //   showMoreButton.remove();
+//     //   isShowMoreButtonExist = false;
+//     // }
+
+//     let remainingCards = cardFilmsCount - showingFilmsCount;
+//     if (remainingCards < showingFilmsCountByButton)
+//       showingFilmsCountByButton = remainingCards;
+//     showingFilmsCount = showingFilmsCount + showingFilmsCountByButton;
+//     filmsListContainer.innerHTML = "";
+
+//     // for (let i = 0; i < showingFilmsCount; i++) {
+//     //   render(filmsListContainer, cardFilmTemplate(watchlistFilms[i]));
+//     // }
+//     // const controlsCardFilm = filmsListContainer.querySelectorAll(".film-card");
+//     // controlsCardFilm.forEach((film) => render(film, controlsTemplate()));
+
+//     // // button "Show more"
+//     // const filmsList = filmsContainer.querySelector(".films-list");
+//     // let showMoreButton = filmsList.querySelector(`.films-list__show-more`);
+//     // if (!isShowMoreButtonExist) {
+//     //   render(filmsList, showMoreButtonTemplate());
+//     //   showMoreButtonElement(showMoreButton, filmsListContainer, isShowMoreButtonExist);
+//     // }
+//   });
+// };
+
+// const showMoreButtonElement = (showMoreButton, filmsListContainer,isShowMoreButtonExist) => {
+//   let showingFilmsCount = SHOWING_FILMS_COUNT_ON_START; //5
+//   let cardFilmsCount = WATCHLIST_FILMS_COUNT; //10
+//   let showingFilmsCountByButton = SHOWING_FILMS_COUNT_BY_BUTTON; //5
+
+//   // !!! Неправильно считаются карточки при выводе
+//   showMoreButton.addEventListener(`click`, () => {
+//     const prevFilmsCount = showingFilmsCount;
+//     let remainingCards = cardFilmsCount - showingFilmsCount;
+//     if (remainingCards < showingFilmsCountByButton)
+//       showingFilmsCountByButton = remainingCards;
+//     showingFilmsCount = showingFilmsCount + showingFilmsCountByButton;
+//     watchlistFilms
+//       .slice(prevFilmsCount, showingFilmsCount)
+//       .forEach((film) =>
+//         render(filmsListContainer, cardFilmTemplate(film), `beforeend`)
+//       );
+
+//     if (showingFilmsCount >= cardFilmsCount) {
+//       showMoreButton.remove();
+//       isShowMoreButtonExist = false;
+//     }
+//   });
+// };
