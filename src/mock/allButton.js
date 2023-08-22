f3b08f158ae3e751c65283be13ec8da0990aae63
Alena <vasilevnalena.2012@gmail.com>
2023-08-22 10:14:02 +0400

    Fixed button #allButton (full rendering of changing elements)

--- src/mock/allButton.js
+++ src/mock/allButton.js
@@ -4,8 +4,9 @@ import { showMoreButtonTemplate } from "../components/showMoreButton.js";
 import { generateFilms } from "./cardFilm.js";
 import { controlsTemplate } from "../components/controls.js";
 import { FILMS_CARDS } from "../const.js";
+import { filmsContainerTemplate } from "../components/filmsList.js";
 
-const CARD_FILMS_COUNT = 15;
+const CARD_FILMS_COUNT = 20;
 const SHOWING_FILMS_COUNT_ON_START = 5;
 const SHOWING_FILMS_COUNT_BY_BUTTON = 5;
 
@@ -30,52 +31,32 @@ export const defaultCardOutput = (siteMainElement) => {
   showMoreButtonElement(filmsListContainer, filmsList);
 };
 
-//!!! 1)дублируется кнопка show more по нажатию All
-//    2)неправильно работает вывод карточек по кнопке, переделать счетчики по Лекции 3.
+//3)Поиск кнопки на странице и ее удаление(отдельная функция)
 // button #All
 export const allButtonElement = (siteMainElement) => {
-  let cardFilmsCount = CARD_FILMS_COUNT;
-  let showingFilmsCount = SHOWING_FILMS_COUNT_ON_START;
-  let showingFilmsCountByButton = SHOWING_FILMS_COUNT_BY_BUTTON;
   const allButton = document.getElementById("all");
-  const filmsContainer = siteMainElement.querySelector(".films");
-  const filmsListContainer = filmsContainer.querySelector(
-    ".films-list__container"
-  );
+  let filmsContainer = siteMainElement.querySelector(".films");
 
   allButton.addEventListener(`click`, () => {
-    // showMoreButton.remove();
+    filmsContainer.remove();
+    render(siteMainElement, filmsContainerTemplate());
+    filmsContainer = siteMainElement.querySelector(".films");
 
-    // if (cardFilmsCount <= showingFilmsCount) {
-    //   showingFilmsCount = cardFilmsCount;
-    //   showMoreButton.remove();
-    // }
-    // if(isShowMoreButtonExist)
-    // showMoreButton.remove();
-
-    let remainingCards = cardFilmsCount - showingFilmsCount;
-    if (remainingCards < showingFilmsCountByButton)
-      showingFilmsCountByButton = remainingCards;
-    showingFilmsCount = showingFilmsCount + showingFilmsCountByButton;
-    filmsListContainer.innerHTML = "";
-
-    for (let i = 0; i < showingFilmsCount; i++) {
+    const filmsListContainer = filmsContainer.querySelector(
+      ".films-list__container"
+    );
+    const filmsList = filmsContainer.querySelector(".films-list");
+    for (let i = 0; i < SHOWING_FILMS_COUNT_ON_START; i++) {
       render(filmsListContainer, cardFilmTemplate(films[i]));
     }
-    const controlsCardFilm = filmsListContainer.querySelectorAll(".film-card");
-    controlsCardFilm.forEach((film) => render(film, controlsTemplate()));
 
-   // button "Show more"
-   const filmsList = filmsContainer.querySelector(".films-list");
-   render(filmsList, showMoreButtonTemplate());
-   // let showMoreButton = filmsList.querySelector(`.films-list__show-more`);
-   // showMoreButtonElement(showMoreButton, filmsListContainer);
-   showMoreButtonElement(filmsListContainer, filmsList);
+    // button "Show more"
+    render(filmsList, showMoreButtonTemplate());
+    showMoreButtonElement(filmsListContainer, filmsList);
   });
 };
 
 const showMoreButtonElement = (filmsListContainer, filmsList) => {
-  // const showMoreButtonElement = (showMoreButton, filmsListContainer) => {
   let showingFilmsCount = SHOWING_FILMS_COUNT_ON_START;
   let cardFilmsCount = CARD_FILMS_COUNT;
   let showingFilmsCountByButton = SHOWING_FILMS_COUNT_BY_BUTTON;
@@ -83,9 +64,9 @@ const showMoreButtonElement = (filmsListContainer, filmsList) => {
 
   showMoreButton.addEventListener(`click`, () => {
     const prevFilmsCount = showingFilmsCount;
-    let remainingCards = cardFilmsCount - showingFilmsCount;
-    if (remainingCards < showingFilmsCountByButton)
-      showingFilmsCountByButton = remainingCards;
+    // let remainingCards = cardFilmsCount - showingFilmsCount;
+    // if (remainingCards < showingFilmsCountByButton)
+    //   showingFilmsCountByButton = remainingCards;
     showingFilmsCount = showingFilmsCount + showingFilmsCountByButton;
     films
       .slice(prevFilmsCount, showingFilmsCount)
